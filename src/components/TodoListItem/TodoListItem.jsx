c8527b0405eaa32f002f5d41a0631bf0a16e14c1
edellveis <161501144+edellveis@users.noreply.github.com>
2024-10-05 13:03:31 +0300

    todo-list-finsh

--- src/components/TodoListItem/TodoListItem.jsx
+++ src/components/TodoListItem/TodoListItem.jsx
@@ -1,3 +1,16 @@
-export const TodoListItem = () => {
-  return <h3>TodoListItem</h3>;
+import style from './TodoListItem.module.css';
+import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
+import { Text } from 'components';
+export const TodoListItem = ({text, count, removeTodo, id}) => {
+  return (
+    <div className={style.box}>
+      <Text textAlign="center" marginBottom="20">
+        TODO #{count}
+      </Text>
+      <Text>{text}</Text>
+      <button className={style.deleteButton} type="button" onClick={()=> removeTodo(id)}>
+        <RiDeleteBinLine size={24} />
+      </button>
+    </div>
+  );
 };
